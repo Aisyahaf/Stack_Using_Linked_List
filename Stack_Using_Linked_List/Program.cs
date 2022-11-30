fd3986a874fc8fb157bdb0358ab83383f49a6add
Aisah Fitriani <aisah.atik.ft21@mail.umy.ac.id>
2022-11-30 16:55:49 +0700

    Add Method Public void push

--- Stack_Using_Linked_List/Program.cs
+++ Stack_Using_Linked_List/Program.cs
@@ -33,5 +33,17 @@ namespace Stack_Using_Linked_List
             else
                 return (false);
         }
+        public void push(int element)
+        {
+            Node fresh;
+            fresh = new Node(element, null);
+
+            fresh.next = top;
+            top = fresh;
+            Console.WriteLine("\n" + element + " pushed.");
+        }
+
+
+
     }
 }
